d1b6aba02887cb490e4b739198dd8efd4fa5f384
liandaraaa <lianda.ramadhana31@gmail.com>
2021-08-12 22:25:41 +0700

    finish get data from json

---
+++ android/app/src/main/kotlin/com/steamy/restaurant_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.steamy.restaurant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/restaurant.dart
@@ -0,0 +1,134 @@
+import 'dart:convert';
+
+class RestaurantData {
+  late List<Restaurants> restaurants;
+
+  RestaurantData({required this.restaurants});
+
+  RestaurantData.fromJson(Map<String, dynamic> json) {
+    if (json['restaurants'] != null) {
+      restaurants = <Restaurants>[];
+      json['restaurants'].forEach((v) {
+        restaurants.add(new Restaurants.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['restaurants'] = this.restaurants.map((v) => v.toJson()).toList();
+    return data;
+  }
+}
+
+RestaurantData parseRestaurantData(String? json) {
+  if (json == null) {
+    return RestaurantData(restaurants: List.empty());
+  }
+
+  return RestaurantData.fromJson(jsonDecode(json));
+}
+
+class Restaurants {
+  late String id;
+  late String name;
+  late String description;
+  late String pictureId;
+  late String city;
+  late num rating;
+  late Menus menus;
+
+  Restaurants(
+      {required this.id,
+      required this.name,
+      required this.description,
+      required this.pictureId,
+      required this.city,
+      required this.rating,
+      required this.menus});
+
+  Restaurants.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    description = json['description'];
+    pictureId = json['pictureId'];
+    city = json['city'];
+    rating = json['rating'];
+    menus = (json['menus'] != null ? new Menus.fromJson(json['menus']) : null)!;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['description'] = this.description;
+    data['pictureId'] = this.pictureId;
+    data['city'] = this.city;
+    data['rating'] = this.rating;
+    if (this.menus != null) {
+      data['menus'] = this.menus.toJson();
+    }
+    return data;
+  }
+}
+
+class Menus {
+  late List<Foods> foods;
+  late List<Drinks> drinks;
+
+  Menus({required this.foods, required this.drinks});
+
+  Menus.fromJson(Map<String, dynamic> json) {
+    if (json['foods'] != null) {
+      foods = <Foods>[];
+      json['foods'].forEach((v) {
+        foods.add(new Foods.fromJson(v));
+      });
+    }
+    if (json['drinks'] != null) {
+      drinks = <Drinks>[];
+      json['drinks'].forEach((v) {
+        drinks.add(new Drinks.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['foods'] = this.foods.map((v) => v.toJson()).toList();
+    data['drinks'] = this.drinks.map((v) => v.toJson()).toList();
+    return data;
+  }
+}
+
+class Drinks {
+  late String name;
+
+  Drinks({required this.name});
+
+  Drinks.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    return data;
+  }
+}
+
+class Foods {
+  late String name;
+
+  Foods({required this.name});
+
+  Foods.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    return data;
+  }
+}
