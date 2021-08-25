28d4fd27f52e004808e329b2cf56dec8437321d8
liandaraaa <lianda.ramadhana31@gmail.com>
2021-08-25 16:18:28 +0700

    handle empty review

--- lib/ui/customer_reviews_page.dart
+++ lib/ui/customer_reviews_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/data/model/restaurant.dart';
+import 'package:restaurant_app/widgets/error_widget.dart';
 
 class CustomerReviewsPage extends StatelessWidget {
   static const routName = '/customer_review_page';
@@ -12,25 +13,34 @@ class CustomerReviewsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Review"),
-      ),
-      body: ListView.builder(
+        appBar: AppBar(
+          title: Text("Review"),
+        ),
+        body: _buildPage(context)
+    );
+  }
+
+  Widget _buildPage(BuildContext context) {
+    if (reviews.isNotEmpty) {
+      return ListView.builder(
         shrinkWrap: true,
         itemCount: reviews.length,
         itemBuilder: (context, index) {
           var review = reviews[index];
           return _buildReviewItem(context, review);
         },
-      ),
-    );
+      );
+    } else {
+      return ErrorWidgetPage(
+        message: "Empty Data", image: 'images/ic_empty.png',);
+    }
   }
 
   Widget _buildReviewItem(BuildContext context, CustomerReview review) {
     return Material(
       child: ListTile(
         contentPadding:
-            const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         title: Container(
           margin: EdgeInsets.only(bottom: 10),
           child: Text(
