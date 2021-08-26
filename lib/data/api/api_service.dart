d8e6687ec34b4bb47175f41d7818dd210906ebdf
liandaraaa <lianda.ramadhana31@gmail.com>
2021-08-26 08:20:47 +0700

    set error no interenet

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:restaurant_app/data/model/restaurant.dart';
@@ -7,32 +8,44 @@ class ApiService {
   static final String _baseUrl = 'https://restaurant-api.dicoding.dev/';
 
   Future<RestaurantListResponse> getRestaurants() async {
-    var url = Uri.parse(_baseUrl + "/list");
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      return RestaurantListResponse.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load list of restaurants');
+    try{
+      var url = Uri.parse(_baseUrl + "/list");
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        return RestaurantListResponse.fromJson(json.decode(response.body));
+      } else {
+        throw Exception('Failed to load list of restaurants');
+      }
+    } on SocketException catch (_) {
+      throw Exception("No internet connection");
     }
   }
 
   Future<RestaurantDetailResponse> getRestaurantDetail(String id) async {
-    var url = Uri.parse(_baseUrl + "/detail/$id");
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      return RestaurantDetailResponse.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load detail restaurant');
+    try {
+      var url = Uri.parse(_baseUrl + "/detail/$id");
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        return RestaurantDetailResponse.fromJson(json.decode(response.body));
+      } else {
+        throw Exception('Failed to load detail restaurant');
+      }
+    }on SocketException catch (_) {
+      throw Exception("No internet connection");
     }
   }
 
   Future<RestaurantSearchResponse> getSearchRestaurants(String query) async {
-    var url = Uri.parse(_baseUrl + "/search?q=$query");
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      return RestaurantSearchResponse.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load list of restaurants');
+    try{
+      var url = Uri.parse(_baseUrl + "/search?q=$query");
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        return RestaurantSearchResponse.fromJson(json.decode(response.body));
+      } else {
+        throw Exception('Failed to load list of restaurants');
+      }
+    }on SocketException catch (_) {
+      throw Exception("No internet connection");
     }
   }
 }
