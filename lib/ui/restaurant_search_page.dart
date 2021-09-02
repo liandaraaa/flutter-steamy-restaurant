94f92f0f176e23cd197b626656139792967d9d82
liandaraaa <lianda.ramadhana31@gmail.com>
2021-09-02 19:32:59 +0700

    fix item search

--- lib/ui/restaurant_search_page.dart
+++ lib/ui/restaurant_search_page.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_app/common/state.dart';
 import 'package:restaurant_app/data/api/api_service.dart';
-import 'package:restaurant_app/data/model/restaurant.dart';
 import 'package:restaurant_app/data/provider/restaurant_search_provider.dart';
-import 'package:restaurant_app/ui/restaurant_detail_page.dart';
 import 'package:restaurant_app/widgets/error_widget.dart';
 import 'package:restaurant_app/widgets/platform_widgets.dart';
+import 'package:restaurant_app/widgets/restaurant_item.dart';
 
 class RestaurantSearchPage extends StatefulWidget {
   static const routName = '/restaurant_search';
@@ -47,7 +46,7 @@ class RestaurantSearchPageState extends State<RestaurantSearchPage> {
             itemCount: state.response.restaurants.length,
             itemBuilder: (context, index) {
               var restaurant = state.response.restaurants[index];
-              return _buildRestaurantItem(context, restaurant);
+              return RestaurantItem(restaurant: restaurant);
             },
           );
         } else if (state.state == ResultState.NoData) {
@@ -82,64 +81,6 @@ class RestaurantSearchPageState extends State<RestaurantSearchPage> {
         ),
       ),
       child: _buildList(context),
-    );
-  }
-
-  Widget _buildRestaurantItem(BuildContext context, Restaurant restaurant) {
-    return Material(
-      child: ListTile(
-        contentPadding:
-            const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        leading: ClipRRect(
-            borderRadius: BorderRadius.circular(4.0),
-            child: Image.network(
-              restaurant.pictureId,
-              width: 100,
-              fit: BoxFit.fill,
-            )),
-        title: Container(
-          margin: EdgeInsets.only(bottom: 10),
-          child: Text(
-            restaurant.name,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-        subtitle: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(bottom: 8),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.star,
-                    size: 10,
-                  ),
-                  Text(
-                    restaurant.rating.toString(),
-                    style: TextStyle(fontSize: 10),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.location_pin,
-                  size: 10,
-                ),
-                Text(
-                  restaurant.city,
-                  style: TextStyle(fontSize: 10),
-                ),
-              ],
-            ),
-          ],
-        ),
-        onTap: () {
-          Navigator.pushNamed(context, RestaurantPage.routName,
-              arguments: restaurant);
-        },
-      ),
     );
   }
 }
